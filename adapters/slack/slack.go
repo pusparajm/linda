effe5661e83d51a25fd134be466c1c97aeb982ec
Pavel Kiselev <kpashka@gmail.com>
2015-07-26 17:32:57 +0700

    closes #9

--- adapters/slack/slack.go
+++ adapters/slack/slack.go
@@ -16,14 +16,15 @@ const (
 
 // Slack adapter object
 type Slack struct {
-	api        *wrapper.Slack
-	cfg        config.Adapter
-	channels   []wrapper.Channel
-	chSender   chan wrapper.OutgoingMessage
-	chReceiver chan wrapper.SlackEvent
-	msgParams  wrapper.PostMessageParameters
-	userId     string
-	users      []wrapper.User
+	api         *wrapper.Slack
+	cfg         config.Adapter
+	channels    []wrapper.Channel
+	chSender    chan wrapper.OutgoingMessage
+	chReceiver  chan wrapper.SlackEvent
+	lastPcEvent *wrapper.PresenceChangeEvent
+	msgParams   wrapper.PostMessageParameters
+	userId      string
+	users       []wrapper.User
 }
 
 // Create new Slack adapter instance
@@ -92,32 +93,37 @@ func (adapter *Slack) Listen(events chan *commons.Event) {
 		case msg := <-adapter.chReceiver:
 			switch msg.Data.(type) {
 			case wrapper.HelloEvent:
-				slackEvent := msg.Data.(wrapper.HelloEvent)
-				log.WithField("adapter", adapter.cfg.Type).Debugf("Hello: %v", slackEvent)
+				event := msg.Data.(wrapper.HelloEvent)
+				log.WithField("adapter", adapter.cfg.Type).Debugf("Hello: %v", event)
 
 			case *wrapper.MessageEvent:
-				slackEvent := msg.Data.(*wrapper.MessageEvent)
-				channelName := adapter.getChannelName(slackEvent.ChannelId)
+				event := msg.Data.(*wrapper.MessageEvent)
+				channelName := adapter.getChannelName(event.ChannelId)
 
 				// Operate only on selected channel
 				if channelName == adapter.cfg.Channel {
-					log.WithField("adapter", adapter.cfg.Type).Debugf("Message: %v", slackEvent)
-					e := commons.NewEvent().FromSlackMessage(slackEvent)
-					adapter.syncUsers()
-					e.Username = adapter.getUsername(slackEvent.UserId)
+					log.WithField("adapter", adapter.cfg.Type).Debugf("Message: %v", event)
+					e := commons.NewEvent().FromSlackMessage(event)
+					//adapter.syncUsers()
+					e.Username = adapter.getUsername(event.UserId)
 					events <- e
 				}
 
 			case *wrapper.PresenceChangeEvent:
-				slackEvent := msg.Data.(*wrapper.PresenceChangeEvent)
-				log.WithField("adapter", adapter.cfg.Type).Debugf("Presence Change: %v", slackEvent)
+				event := msg.Data.(*wrapper.PresenceChangeEvent)
+				log.WithField("adapter", adapter.cfg.Type).Debugf("Presence Change: %v", event)
 
 				// Get username for presence change event
-				if slackEvent.UserId != adapter.userId {
-					e := commons.NewEvent().FromSlackStatus(slackEvent)
-					adapter.syncUsers()
-					e.Username = adapter.getUsername(slackEvent.UserId)
-					events <- e
+				if event.UserId != adapter.userId {
+					isDuplicate := adapter.isDuplicateEvent(event)
+					if !isDuplicate {
+						e := commons.NewEvent().FromSlackStatus(event)
+						//adapter.syncUsers()
+						e.Username = adapter.getUsername(event.UserId)
+						events <- e
+					}
+
+					adapter.lastPcEvent = event
 				}
 
 			case wrapper.LatencyReport:
@@ -132,6 +138,19 @@ func (adapter *Slack) Listen(events chan *commons.Event) {
 			}
 		}
 	}
+}
+
+// Slack, for some reason, often send duplicate presence change events
+func (adapter *Slack) isDuplicateEvent(event *wrapper.PresenceChangeEvent) bool {
+	if adapter.lastPcEvent == nil {
+		return false
+	}
+
+	if adapter.lastPcEvent.UserId == event.UserId && adapter.lastPcEvent.Presence == event.Presence {
+		return true
+	}
+
+	return false
 }
 
 // Send message
